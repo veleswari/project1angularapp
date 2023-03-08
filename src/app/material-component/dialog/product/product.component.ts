869ffae776557dd9d6411a536dfa5177a95ffbcd
veleswari <veleswaripaluru@gmail.com>
2023-03-08 12:52:05 +0530

    project1

---
+++ src/app/material-component/dialog/product/product.component.ts
@@ -0,0 +1,85 @@
+import { Component, EventEmitter, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { GlobalConstants } from 'src/app/shared/global-constant';
+
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  onAddProduct = new EventEmitter();
+  onEditProduct = new EventEmitter();
+  productForm:any = FormGroup;
+  dialogAction:any ="Add";
+  action:any = "Add";
+  responseMessage:any;
+  categories:any = [];
+
+  constructor(@Inject(MAT_DIALOG_DATA) public dialogData:any,
+  private formBuilder:FormBuilder,
+  private productService:ProductService,
+  public dialogRef:MatDialogRef<ProductComponent>,
+  private categoryService:CategoryService,
+  private snackbarService:SnackbarService) { }
+
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      name:[null,[Validators.required,Validators.pattern(GlobalConstants.nameRegex)]],
+      categoryId:[null,[Validators.required]],
+      price:[null,[Validators.required]],
+      description:[null,[Validators.required]]
+    });
+    if(this.dialogAction.action === "Edit") {
+      this.dialogAction = "Edit";
+      this.action = "Update";
+      this.productForm.patchvalue(this.dialogData.data);
+    }
+    this.getCategories();
+  }
+  getCategories(){
+    this.categoryService.getCategories().subscribe((response)=>{
+      this.categories = response;
+    },(error:any)=>{
+      console.log(error);
+      if(error.error?.message){
+        this.responseMessage = error.error?.message;
+      }else{
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackBar(this.responseMessage,GlobalConstants.error);
+    })
+  }
+  
+  handleSubmit(){
+    if(this.dialogAction === "Edit"){
+      this.edit();
+    }else{
+      this.add();
+    }
+  }
+  add(){
+    var formData = this.productForm.value;
+    var data ={
+      name: formData.name,
+      categoryId:formData.categoryId,
+      price:formData.price,
+      description:formData.description
+    }
+
+    this.productService.add(data).subscribe((response:any)=>{
+      this.dialogRef.close();
+      this.onAddProduct.emit();
+      this.responseMessage = response.message;
+      this.snackbarService.openSnackBar(this.responseMessage,"success");
+    })
+  }
+
+  edit(){}
+}
